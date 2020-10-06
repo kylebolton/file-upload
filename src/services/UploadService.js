04a8cc153af0ebd6311c37ebeeafd10d48cb4bc3
Kyle Bolton <hello@kylebolton.me>
2020-10-06 18:52:38 +0000

    Change post to put

--- src/services/UploadService.js
+++ src/services/UploadService.js
@@ -5,7 +5,7 @@ const upload = (file, onUploadProgress) => {
 
   formData.append("file", file);
 
-  return http.post("/upload", formData, {
+  return http.put("/upload", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
